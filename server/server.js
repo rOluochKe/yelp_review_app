16d1f224abc9eefeacacfeb552de1f9e076a162a
roluochKe <rasholuoch@gmail.com>
2022-01-02 11:09:37 +0300

    front end setup

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(express.json())
 app.get('/api/v1/restaurants', async (req, res) => {
   try {
     //const results = await db.query("select * from restaurants");
+
     const restaurantRatingsData = await db.query(
       'select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id;'
     )
@@ -32,14 +33,14 @@ app.get('/api/v1/restaurants', async (req, res) => {
 
 //Get a Restaurant
 app.get('/api/v1/restaurants/:id', async (req, res) => {
-  console.log(req.params.id)
+  // console.log(req.params.id)
+  // select * from restaurants wehre id = req.params.id
 
   try {
     const restaurant = await db.query(
       'select * from restaurants left join (select restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as average_rating from reviews group by restaurant_id) reviews on restaurants.id = reviews.restaurant_id where id = $1',
       [req.params.id]
     )
-    // select * from restaurants wehre id = req.params.id
 
     const reviews = await db.query(
       'select * from reviews where restaurant_id = $1',
@@ -61,7 +62,7 @@ app.get('/api/v1/restaurants/:id', async (req, res) => {
 
 // Create a Restaurant
 app.post('/api/v1/restaurants', async (req, res) => {
-  console.log(req.body)
+  // console.log(req.body)
 
   try {
     const results = await db.query(
@@ -69,6 +70,7 @@ app.post('/api/v1/restaurants', async (req, res) => {
       [req.body.name, req.body.location, req.body.price_range]
     )
     console.log(results)
+
     res.status(201).json({
       status: 'succes',
       data: {
